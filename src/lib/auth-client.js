a1f9d533e29244036e307ac04dd3e85fd7a3dccc
mamun945 <almamunab2001@gmail.com>
2026-05-03 18:25:59 +0600

    final diploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,5 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     baseURL: "https://summer-ecommerce-website.vercel.app"
     // baseURL: "http://localhost:3000"
-
-    // hello 
 })
 export const { signIn, signUp, useSession } = createAuthClient()
